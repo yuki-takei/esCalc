a8a360d4db38a3c2e7f0b420c00d498ebbd58319
Yuki Takei <yuki@weseek.co.jp>
2015-08-30 10:07:23 +0900

    impl keyboard event handlers

--- src/app/components/main/main-controller.ts
+++ src/app/components/main/main-controller.ts
@@ -2,20 +2,76 @@ import 'ace';
 import 'ace/theme-github';
 import 'ace/mode-javascript';
 
-class MainController {
 
-  const ACE_BASE_PATH = "libs/ace@1.2.0/";
+class MainController {
+  static ACE_BASE_PATH: String = "libs/ace@1.2.0/";
 
   constructor() {
     this.initAce();
   }
 
   private initAce(): void {
-    var editor = ace.edit("ec-ace-editor");
-    ace.config.set("basePath", ACE_BASE_PATH);
+    var thisObj: MainController = this;
+
+    var editor: AceAjax.Editor = ace.edit("ec-ace-editor");
+    ace.config.set("basePath", MainController.ACE_BASE_PATH);
     editor.setTheme("ace/theme/github");
     editor.getSession().setMode("ace/mode/javascript");
+
+    editor.commands.addCommands([
+      // bind to Enter
+      <AceAjax.EditorCommand> {
+        name: 'enterCommand',
+        bindKey: {win: 'Enter',  mac: 'Enter'},
+        exec: function(editor: AceAjax.Editor) {
+          thisObj.enterHandler(editor);
+        },
+        readOnly: false
+      },
+      // bind to Shift+Enter
+      <AceAjax.EditorCommand> {
+        name: 'shiftEnterCommand',
+        bindKey: {win: 'Shift+Enter',  mac: 'Shift+Enter'},
+        exec: function(editor: AceAjax.Editor) {
+          thisObj.shiftEnterHandler(editor);
+        },
+        readOnly: false
+      },
+      // bind to Ctrl+Enter
+      <AceAjax.EditorCommand> {
+        name: 'ctrlEnterCommand',
+        bindKey: {win: 'Ctrl+Enter',  mac: 'Cmd+Enter'},
+        exec: function(editor: AceAjax.Editor) {
+          thisObj.ctrlEnterHandler(editor);
+        },
+        readOnly: false
+      }
+    ]);
   }
+
+  private insertNewLine(editor: AceAjax.Editor): void {
+    var currentPos = editor.getCursorPosition();
+    editor.getSession().getDocument().insertMergedLines(currentPos, ['', '']);
+  }
+
+  private enterHandler(editor: AceAjax.Editor): void {
+    console.log("Enter");
+
+    // TODO evaluate current line
+
+    this.insertNewLine(editor);
+  }
+
+  private shiftEnterHandler(editor: AceAjax.Editor): void {
+    this.insertNewLine(editor);
+  }
+
+  private ctrlEnterHandler(editor: AceAjax.Editor): void {
+    console.log("Ctrl + Enter");
+
+    // TODO evaluate all
+  }
+
 }
 
 export default MainController;
