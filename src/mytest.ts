4885ffc13a6c57cdfcada6c5da17590a19efa1b8
Yuki Takei <yuki@weseek.co.jp>
2016-04-21 03:21:53 +0900

    update tslint.json

--- src/mytest.ts
+++ src/mytest.ts
@@ -1,10 +1,10 @@
 export namespace mytest {
-  'use strict';
+  'use strict'
 
   export class SomeClass {
 
       static someMethod(): {c: number, d: string} {
-          return {c: 0, d: 'abc'};
+          return {c: 0, d: 'abc'}
       }
   }
 }
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,17 +1,15 @@
 /// <reference path="../.tmp/typings/bundle.d.ts" />
 
-import * as assert from 'power-assert';
-import {mytest} from '../src/mytest';
+import * as assert from 'power-assert'
+import {mytest} from '../src/mytest'
 
-describe("Some Class", ()=>{
-    context("someMethod()", ()=>{
+describe("Some Class", () => {
+    context("someMethod()", () => {
 
-        it("should return object", ()=>{
-
-          assert(mytest.SomeClass != null);
-            const a = mytest.SomeClass.someMethod();
-            const b = {c: 0, d: "abc"};
-            assert.deepEqual(a, b);
-        });
+      it("should return object", () => {
+        const a = mytest.SomeClass.someMethod()
+        const b = {c: 0, d: "abc"}
+        assert.deepEqual(a, b)
+      });
     });
 });
