203c5d053524c598508db0df53cc6815508df82f
Yuki Takei <yuki@weseek.co.jp>
2016-04-21 03:11:32 +0900

    add sample test codes

---
+++ src/mytest.ts
@@ -0,0 +1,10 @@
+export namespace mytest {
+  'use strict';
+
+  export class SomeClass {
+
+      static someMethod(): {c: number, d: string} {
+          return {c: 0, d: 'abc'};
+      }
+  }
+}
---
+++ test/sample-test.ts
@@ -0,0 +1,17 @@
+/// <reference path="../.tmp/typings/bundle.d.ts" />
+
+import * as assert from 'power-assert';
+import {mytest} from '../src/mytest';
+
+describe("Some Class", ()=>{
+    context("someMethod()", ()=>{
+
+        it("should return object", ()=>{
+
+          assert(mytest.SomeClass != null);
+            const a = mytest.SomeClass.someMethod();
+            const b = {c: 0, d: "abc"};
+            assert.deepEqual(a, b);
+        });
+    });
+});
